a220cba9d9525fd9c4d7b151a9a91f9407656262
Zixi Qiao <qiaozixi@gmail.com>
2024-10-27 19:50:38 -0400

    tested exceptions

--- SuperArrayExceptionTester.java
+++ SuperArrayExceptionTester.java
@@ -41,11 +41,88 @@ public class SuperArrayExceptionTester{
     }
 
     //Test3: set negative index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "set -1 [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.set(-1, "test");
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test4: set size() index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "set size() [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.set(list.size(), "test");
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test5: add negative index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "add -1 [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.add(-1, "test");
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test6: add size()+1 index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "add size()+1 [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.add(list.size()+1, "test");
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test7: remove negative index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "remove -1 [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.remove(-1);
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test8: remove size() index [IndexOutOfBoundsException]
+    testNum++;
+    testName = "remove size() [IndexOutOfBoundsException]";
+    list = init();
+    try{
+        list.remove(list.size());
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IndexOutOfBoundsException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
     //Test9: constructor negative capacity [IllegalArgumentException]
+    testNum++;
+    testName = "constructor -1 [IllegalArgumentException]";
+    list = init();
+    try{
+        list = new SuperArray(-3);
+        System.out.println(testNum+". FAIL! No exception thrown "+testName);
+    }catch(IllegalArgumentException e){
+      System.out.println(testNum+". PASS "+testName);
+    }catch(Exception e){
+      System.out.println(testNum+". FAIL! wrong exception thrown "+testName);
+    }
   }
 }
